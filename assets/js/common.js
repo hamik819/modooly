99efa1980409b9c6085b541eb978f74657333990
hami <hamik@limefriends.com>
2023-06-15 12:14:10 +0900

    메인 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,39 +13,38 @@ function bannerSlide() {
 		arrows: false,
 	});
 }
-function infoSlide() {
-	var slickElement = $('.main__info--slide');
-	var progressSection = $('.main__info--progress');
-	var progressBar = $('.main__info--progress span');
+// function infoSlide() {
+// 	var slickElement = $('.main__info--slide');
+// 	var progressSection = $('.main__info--progress');
+// 	var progressBar = $('.main__info--progress span');
 
-	slickElement.on('init reInit', function (event, slick, currentSlide, nextSlide) {
-	var i = (currentSlide ? currentSlide : 0) + 1;
-	var currentPercent = (100 / slick.slideCount) * i + '%';
-	progressBar.css({width: currentPercent});
-	if(currentPercent === '100%'){
-		progressSection.addClass('full');
-	} else {
-		progressSection.removeClass('full');
-	}
-	});
-	slickElement.on('beforeChange', function (event, slick, currentSlide, nextSlide){
-		var i = (nextSlide + 1);
-		var currentPercent = (100 / slick.slideCount) * i + '%';
-		progressBar.css({width: currentPercent});
-		if(currentPercent === '100%'){
-			progressSection.addClass('full');
-		} else {
-			progressSection.removeClass('full');
-		}
-	});
-	slickElement.slick({
-		slidesToShow: 1,
-		arrows: false,
-	});
-}
+// 	slickElement.on('init reInit', function (event, slick, currentSlide, nextSlide) {
+// 	var i = (currentSlide ? currentSlide : 0) + 1;
+// 	var currentPercent = (100 / slick.slideCount) * i + '%';
+// 	progressBar.css({width: currentPercent});
+// 	if(currentPercent === '100%'){
+// 		progressSection.addClass('full');
+// 	} else {
+// 		progressSection.removeClass('full');
+// 	}
+// 	});
+// 	slickElement.on('beforeChange', function (event, slick, currentSlide, nextSlide){
+// 		var i = (nextSlide + 1);
+// 		var currentPercent = (100 / slick.slideCount) * i + '%';
+// 		progressBar.css({width: currentPercent});
+// 		if(currentPercent === '100%'){
+// 			progressSection.addClass('full');
+// 		} else {
+// 			progressSection.removeClass('full');
+// 		}
+// 	});
+// 	slickElement.slick({
+// 		slidesToShow: 1,
+// 		arrows: false,
+// 	});
+// }
 $(document).ready(function() {
 	if( $('.main').length > 0 ) {
 		bannerSlide();
-		infoSlide();
 	}
 });
